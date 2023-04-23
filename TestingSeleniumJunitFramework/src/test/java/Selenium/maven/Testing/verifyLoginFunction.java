835b3cd8c718e98263e331cc8394f88fc516b107
nandhu19940 <nandha008kumar@gmail.com>
2023-04-23 13:35:16 +0530

    Message edit changes

--- TestingSeleniumJunitFramework/src/test/java/Selenium/maven/Testing/verifyLoginFunction.java
+++ TestingSeleniumJunitFramework/src/test/java/Selenium/maven/Testing/verifyLoginFunction.java
@@ -18,23 +18,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class verifyLoginFunction {
-	
+
 	private static ChromeDriver driver;
 	WebElement element;
-	
+
 	@BeforeClass
-	public static void openBrowser(){
+	public static void openBrowser() {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
-	driver = new ChromeDriver(options);
-	
+		driver = new ChromeDriver(options);
+
 	}
 
 	@Before
 	public void launchWebsite() {
 		driver.get("https://www.saucedemo.com/");
 	}
-	
+
 	@Test
 	public void loginToSauceDemo() {
 		String pageTitle = driver.getTitle();
@@ -46,27 +46,28 @@ public class verifyLoginFunction {
 		username.sendKeys("standard_user");
 		password.sendKeys("secret_sauce");
 		loginButton.click();
-		
+
 		final WebDriverWait waitForDashBoradPageElement = new WebDriverWait(driver, Duration.ofSeconds(10));
-		waitForDashBoradPageElement.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/span[contains(text(),'Products')]")));
-		WebElement dashBoradPageConfirmation = driver.findElement(By.xpath("//div[2]/span[contains(text(),'Products')]"));
+		waitForDashBoradPageElement.until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("//div[2]/span[contains(text(),'Products')]")));
+		WebElement dashBoradPageConfirmation = driver
+				.findElement(By.xpath("//div[2]/span[contains(text(),'Products')]"));
 		String dashBoradPageTextVerify = dashBoradPageConfirmation.getText();
-		if(dashBoradPageTextVerify.equals("Products")) {
-			System.out.println("Successfully entered in to dashboard page");
+		if (dashBoradPageTextVerify.equals("Products")) {
+			System.out.println("Successfully entered in to dashboard page and verified the element");
 		}
 	}
-	
-	
+
 	@After
 	public void closeBrowserSession() {
 		driver.quit();
 	}
-	
-	public static void main(String[] args) {									
-	      Result result = JUnitCore.runClasses(verifyLoginFunction.class);					
-				for (Failure failure : result.getFailures()) {							
-	         System.out.println(failure.toString());					
-	      }		
-	      System.out.println("Result=="+result.wasSuccessful());							
-	   }	
+
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(verifyLoginFunction.class);
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		System.out.println("Result==" + result.wasSuccessful());
+	}
 }
